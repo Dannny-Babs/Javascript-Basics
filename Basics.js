e1a7c2ad223ad9bd03e8515fbed40f725c1b62f7
Dannny-Babs <dannybabs123456@gmail.com>
2024-02-01 21:16:43 +0100

    Created main html file and added CSS files

--- Basics.js
+++ Basics.js
@@ -1,1 +1,19 @@
 console.log("Hello World");
+
+let b = "smoothie";
+console.log(b);
+
+let someNumber = 45;
+console.log(someNumber);
+
+let s = Math.random() *10;
+s= Math.floor(s);
+
+console.log(s);
+
+let x = 'Die for the 10th';
+let part = x.at(0);
+console.log(x);
+console.log(part);
+
+
